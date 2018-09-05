5b61f14b3fd2a04a5ed5d183086c7acae5108610
Truman Larson <truman.larson@gmail.com>
2018-09-05 14:26:04 -0400

    Finished Velocity and made updates to the world state

--- RacketEngine.rkt
+++ RacketEngine.rkt
@@ -19,9 +19,9 @@
 ;interp. position contains an x and y coordinate, (x,y) is center
 (define-struct position (x y))
 
-;Vector is (make-vector Number Number[0,360])
-;interp. vector contains magnitude and angle in degrees
-(define-struct vector (magnitude angle))
+;Velocity is (make-velocity Number Number)
+;interp. velocity vector that  contains two components x, y
+(define-struct velocity (dx dy))
 
 ;Entity is (make-entity (make-position Number Number) Number [0, infinity) image vector)
 ;interp. entity contains a position struct, mass, the image to draw, and a velocity vector.
@@ -34,9 +34,9 @@
 ;WS is (make-WS ListOfEntities)
 ;interp. WS is the worldstate, contains list of all entities in the world.
 (define-struct WS (listOfEntities))
-(define loe (list (make-entity (make-position 100 100) 100 (circle 100 "solid" "red") (make-vector 0 0))
-                  (make-entity (make-position 500 50) 50 (square 100 "solid" "red") (make-vector 0 0))))
-
+(define loe (list (make-entity (make-position 100 100) 100 (circle 100 "solid" "red") (make-velocity 10 90))
+                  (make-entity (make-position 500 50) 50 (square 100 "solid" "red") (make-velocity 40 45))))
+(define WS-ex (make-WS loe))
 
 ;; =================
 ;; Data Definitions:
@@ -64,20 +64,45 @@
 ;; 
 (define (main ws)
   (big-bang ws                   ; WS
-            (on-tick   tock)     ; WS -> WS
-            (to-draw   render)   ; WS -> Image
-            (stop-when ...)      ; WS -> Boolean
-            (on-mouse  ...)      ; WS Integer Integer MouseEvent -> WS
-            (on-key    ...)))    ; WS KeyEvent -> WS
+    (on-tick   tock)     ; WS -> WS
+    (to-draw   render)   ; WS -> Image
+    (stop-when ...)      ; WS -> Boolean
+    (on-mouse  ...)      ; WS Integer Integer MouseEvent -> WS
+    (on-key    ...)))    ; WS KeyEvent -> WS
 
 ;; WS -> WS
 ;; produce the next ...
-;; !!!
-(define (tock ws) ...)
 
+(define (tock ws)
+  (tick-velocity ws))
 
+;;tick-velocity
+;; WS->WS
+;; updates all the positions for the given list of entities based on their velocity
+(check-expect (tick-velocity WS-ex)
+              (make-WS (list
+               (make-entity (make-position 110 190) 100 (circle 100 "solid" "red") (make-velocity 10 90))
+               (make-entity (make-position 504 50) 50 (square 100 "solid" "red") (make-velocity 4 0)))))
+
+;(define (tick-velocity WS) (make-WS empty)) ; stub
+
+(define (tick-velocity WS)
+  (cond [(empty? (WS-listOfEntities WS)) empty]
+        [else
+         (make-WS
+          (cons (make-entity
+                   (make-position
+                    (+ (position-x  (entity-position (first (WS-listOfEntities WS))))
+                       (velocity-dx (entity-velocity (first (WS-listOfEntities WS)))))
+                    (+ (position-y  (entity-position (first (WS-listOfEntities WS))))
+                       (velocity-dy (entity-velocity (first (WS-listOfEntities WS))))))
+                   (entity-mass (first (WS-listOfEntities WS)))
+                   (entity-sprite (first (WS-listOfEntities WS)))
+                   (entity-velocity (first (WS-listOfEntities WS))))
+                  (tick-velocity (rest (WS-listOfEntities WS)))))]))
+ 
 ;; WS -> Image
-;; render ...
+;; render a list of entities
 (check-expect (render (make-WS loe)) (place-image (place-image
                                                    (entity-sprite (second loe))
                                                    (position-x (entity-position (second loe)))
@@ -89,7 +114,7 @@
                                                   (/ WIDTH 2)
                                                   (/ HEIGHT 2)
                                                   MTS))
-;; !!!
+
 (define (render ws)
   (place-image (compile-image (WS-listOfEntities ws)) (/ WIDTH 2) (/ HEIGHT 2) MTS))
 
